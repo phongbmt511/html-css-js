3c67c5374aaeec15dbceb3f9ba2867290464770b
phongbmt511 <phongbmt178@gmail.com>
2023-02-18 10:05:48 +0700

    code

---
+++ Modal Popup/app.js
@@ -0,0 +1,16 @@
+var btnOpen = document.querySelector('.open-modal-btn')
+var modal = document.querySelector('.modal')
+var btnClose = document.querySelector('.modal__footer button')
+var iconClose = document.querySelector('.modal__header i')
+function toggleModal(e) {
+    console.log(e.target);
+    modal.classList.toggle('hide')
+}
+btnOpen.addEventListener('click', toggleModal)
+btnClose.addEventListener('click', toggleModal)
+iconClose.addEventListener('click', toggleModal)
+modal.addEventListener('click', function (e) {
+    if (e.target == e.currentTarget) {
+        toggleModal()
+    }
+})
